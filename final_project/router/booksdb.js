dc23990543a5b91004c977dcea23dd8b32f45f95
Vahid Dabbagh <dabbaghvahid@gmail.com>
2025-05-29 14:49:44 -0400

    adding books by title, isbn, author and reviews by book isbn

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,5 +1,5 @@
 let books = [
-      {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "AG8786" },
       {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
       {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,26 +17,47 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let book = books.find(book => book.isbn === req.params.isbn)
+
+  if(!book){
+    return res.status(404).json({message: "No book found!"});
+  }
+
+  return res.json(book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book = books.find(book => book.author === req.params.author)
+
+    if(!book){
+        return res.status(404).json({message: "No book found!"});
+    }
+
+    return res.json(book);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book = books.find(book => book.title === req.params.title)
+
+    if(!book){
+        return res.status(404).json({message: "No book found!"});
+    }
+
+    return res.json(book);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book = books.find(book => book.isbn === req.params.isbn)
+    let reviews = book.reviews
+
+    if(!reviews){
+        return res.status(404).json({message: "No review found!"});
+    }
+
+    return res.json(reviews);
 });
 
 module.exports.general = public_users;

44bb5f797b40390355ccc21ae6ecd2810f256d8c
vahiddabbagh <dabbaghvahid@gmail.com>
2025-05-30 05:35:23 -0400

    final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,36 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+let readBooks = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(books)
+    },1000)})
+
+function readBooksbyISBN(isbn){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const book = books.find(book => book.isbn == isbn)
+            resolve(book)
+        },1000)})
+}
+
+
+function readBooksbyAuthor(author){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const book = books.find(book => book.author == author)
+            resolve(book)
+        },1000)})
+}
+
+
+function readBooksbyTitle(title){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const book = books.find(book => book.title == title)
+            resolve(book)
+        },1000)})
+}
 
 public_users.post("/register", (req,res) => {
 
@@ -29,13 +59,16 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+public_users.get('/',async function (req, res) {
+    const books = await readBooks
+    return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  let book = books.find(book => book.isbn === req.params.isbn)
+public_users.get('/isbn/:isbn',async function (req, res) {
+
+    const book = await readBooksbyISBN(req.params.isbn)
+//   let book = books.find(book => book.isbn === req.params.isbn)
 
   if(!book){
     return res.status(404).json({message: "No book found!"});
@@ -45,8 +78,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    let book = books.find(book => book.author === req.params.author)
+public_users.get('/author/:author', async function (req, res) {
+
+    const book = await readBooksbyAuthor(req.params.author)
+
+    // let book = books.find(book => book.author === req.params.author)
 
     if(!book){
         return res.status(404).json({message: "No book found!"});
@@ -56,8 +92,10 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    let book = books.find(book => book.title === req.params.title)
+public_users.get('/title/:title', async function (req, res) {
+    const book = await readBooksbyTitle(req.params.title) 
+
+    // let book = books.find(book => book.title === req.params.title)
 
     if(!book){
         return res.status(404).json({message: "No book found!"});
